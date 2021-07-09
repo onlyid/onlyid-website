5a7705d25290829ef7fc53f2ca42e3f95ada6593
ltb <liangtb@qq.com>
2021-07-09 20:57:44 +0800

    refactor：优化首页banner文案

--- src/components/home/Banner.js
+++ src/components/home/Banner.js
@@ -1,24 +1,62 @@
-import React from "react";
+import React, { PureComponent } from "react";
 import classNames from "classnames";
 import styles from "./Banner.module.css";
+import { Paper, Popper } from "@material-ui/core";
 
-export default function () {
-    return (
-        <div className={classNames("hero", styles.root)}>
-            <div>
-                <h1 className={styles.title}>唯ID是专为面向公众的应用打造的IDaaS解决方案，</h1>
-                <p className={styles.subtitle}>
-                    帮助网站和APP做好用户登录、用户管理和用户权限控制三件事。
-                </p>
+export default class extends PureComponent {
+    state = {
+        anchorEl: null,
+    };
+
+    showTip = (e) => {
+        this.setState({ anchorEl: e.currentTarget });
+    };
+
+    closeTip = () => {
+        this.setState({ anchorEl: null });
+    };
+
+    render() {
+        const { anchorEl } = this.state;
+
+        return (
+            <div className={classNames("hero", styles.root)}>
                 <div>
-                    <a
-                        className="button button--primary button--lg"
-                        href="https://www.onlyid.net/console"
-                    >
-                        开始使用
-                    </a>
+                    <h1 className={styles.title}>
+                        唯ID是面向 公众应用
+                        <span
+                            className="material-icons"
+                            onMouseEnter={this.showTip}
+                            onMouseLeave={this.closeTip}
+                        >
+                            help
+                        </span>{" "}
+                        的IDaaS解决方案，
+                    </h1>
+                    <p className={styles.subtitle}>
+                        帮助网站和APP做好用户登录、用户管理和用户权限控制三件事。
+                    </p>
+                    <div>
+                        <a
+                            className="button button--primary button--lg"
+                            href="https://www.onlyid.net/console"
+                        >
+                            开始使用
+                        </a>
+                    </div>
                 </div>
+
+                <Popper
+                    open={!!anchorEl}
+                    anchorEl={anchorEl}
+                    placement="top"
+                    className={styles.popper1}
+                >
+                    <Paper>
+                        <p>区分于另一类面向企业内部系统，实现员工身份管理的IDaaS方案</p>
+                    </Paper>
+                </Popper>
             </div>
-        </div>
-    );
+        );
+    }
 }
