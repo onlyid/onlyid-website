b50ad82e33643a09337096b634f510a53689d826
ltb <liangtb@qq.com>
2021-07-28 17:27:38 +0800

    refactor: 优化banner文案

--- src/components/home/Banner.js
+++ src/components/home/Banner.js
@@ -6,10 +6,11 @@ import { Paper, Popper } from "@material-ui/core";
 export default class extends PureComponent {
     state = {
         anchorEl: null,
+        tip: null,
     };
 
-    showTip = (e) => {
-        this.setState({ anchorEl: e.currentTarget });
+    showTip = (e, tip) => {
+        this.setState({ anchorEl: e.currentTarget, tip });
     };
 
     closeTip = () => {
@@ -17,7 +18,7 @@ export default class extends PureComponent {
     };
 
     render() {
-        const { anchorEl } = this.state;
+        const { anchorEl, tip } = this.state;
 
         return (
             <div className={classNames("hero", styles.root)}>
@@ -26,20 +27,29 @@ export default class extends PureComponent {
                         唯ID是面向 公众应用
                         <span
                             className="material-icons"
-                            onMouseEnter={this.showTip}
+                            onMouseEnter={(e) => this.showTip(e, "区别于企业内部信息系统")}
                             onMouseLeave={this.closeTip}
                         >
                             help
                         </span>{" "}
-                        的IDaaS解决方案，
+                        的 IDaaS
+                        <span
+                            className="material-icons"
+                            onMouseEnter={(e) =>
+                                this.showTip(e, "Identity as a Service，基于云端的身份管理")
+                            }
+                            onMouseLeave={this.closeTip}
+                        >
+                            help
+                        </span>{" "}
+                        解决方案，
                     </h1>
-                    <p className={styles.subtitle}>
-                        帮助网站和APP做好用户登录、用户管理和用户权限控制三件事。
-                    </p>
+                    <p className={styles.subtitle}>帮助网站和APP做好用户登录和用户管理两件事。</p>
                     <div>
                         <a
                             className="button button--primary button--lg"
                             href="https://www.onlyid.net/console"
+                            target="_blank"
                         >
                             开始使用
                         </a>
@@ -53,7 +63,7 @@ export default class extends PureComponent {
                     className={styles.popper1}
                 >
                     <Paper>
-                        <p>开放给公众注册、使用的网站和APP</p>
+                        <p>{tip}</p>
                     </Paper>
                 </Popper>
             </div>
