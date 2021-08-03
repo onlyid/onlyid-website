c2b5a780daa4ac34489caefefca3b49434a02695
ltb <liangtb@qq.com>
2021-08-03 13:31:11 +0800

    feat: 完成product section

---
+++ src/components/home/Product.js
@@ -0,0 +1,46 @@
+import React from "react";
+import styles from "./Product.module.css";
+import classNames from "classnames";
+import Link from "@docusaurus/Link";
+
+export default function () {
+    return (
+        <section className={styles.root}>
+            <div className="container">
+                <h2 className="main-title">一站式身份服务</h2>
+                <div className="row">
+                    <div className="col col--4 padding--md">
+                        <Link className={styles.item} to="/otp">
+                            <span className={classNames("material-icons", styles.pin)}>pin</span>
+                            <h3>无密码、轻认证</h3>
+                            <p>用短信/邮件验证码替代传统密码，即使新用户也可以快捷访问你的应用。</p>
+                        </Link>
+                    </div>
+                    <div className="col col--4 padding--md">
+                        <Link className={styles.item} to="/sso">
+                            <span className={classNames("material-icons", styles.portrait)}>
+                                portrait
+                            </span>
+                            <h3>统一账号、单点登录</h3>
+                            <p>
+                                接入统一认证中心，跨应用共享账号，在别处注册的用户可以直接登录你的应用。
+                            </p>
+                        </Link>
+                    </div>
+                    <div className="col col--4 padding--md">
+                        <Link className={styles.item} to="/uam">
+                            <span className={classNames("material-icons", styles.groups)}>
+                                groups
+                            </span>
+                            <h3>用户管理、权限控制</h3>
+                            <p>
+                                使用控制台和Open
+                                API简单快捷地管理用户，为不同的用户灵活分配访问权限。
+                            </p>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import Layout from "@theme/Layout";
 import Banner from "../components/home/Banner";
 import Intro from "../components/home/Intro";
+import Product from "../components/home/Product";
 
 export default function Home() {
     return (
         <Layout title="首页">
             <Banner />
             <Intro />
+            <Product />
         </Layout>
     );
 }
