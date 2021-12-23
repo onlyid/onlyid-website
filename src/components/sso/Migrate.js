d05e50f4fb451179b5480f9732c50e923f5e77a1
ltb <liangtb@qq.com>
2021-12-23 16:45:19 +0800

    feat: 完成sso migrate section

--- src/components/sso/Migrate.js
+++ src/components/sso/Migrate.js
@@ -3,6 +3,7 @@ import React from "react";
 import { Tooltip } from "@material-ui/core";
 import migrate1 from "@site/static/img/sso-migrate1.png";
 import migrate2 from "@site/static/img/sso-migrate2.png";
+import migrate3 from "@site/static/img/sso-migrate3.png";
 import classNames from "classnames";
 
 export default function () {
@@ -25,16 +26,16 @@ export default function () {
                     </span>{" "}
                     的特点，你的应用可以很简单地迁移到SSO。
                 </p>
-                <div className="row">
-                    <div className={classNames("col", styles.left)}>
+                <div className={classNames("row", styles.row1)}>
+                    <div className="col">
                         <img src={migrate1} alt="migrate1" className={styles.img1} />
                     </div>
-                    <div className={classNames("col", styles.right)}>
+                    <div className="col">
                         <p className={styles.title}>
-                            <span className={classNames(styles.step, styles.first)}>1</span>
+                            <span className={styles.num}>1</span>
                             将自有登录替换成 "用唯ID登录"，并增加提示文案引导用户。
                         </p>
-                        <div className={styles.guideBox} style={{ display: "flex" }}>
+                        <div className={styles.guideBox}>
                             <span className="material-icons">thumb_up</span>
                             <p>
                                 建议文案：已迁移用唯ID登录，老用户请使用之前的手机号/邮箱登录，即可自动匹配到原来的用户数据。
@@ -42,13 +43,13 @@ export default function () {
                         </div>
                     </div>
                 </div>
-                <div className="row">
-                    <div className={classNames("col", styles.left)}>
+                <div className={classNames("row", styles.row2)}>
+                    <div className="col">
                         <img src={migrate2} alt="migrate2" className={styles.img1} />
                     </div>
-                    <div className={classNames("col", styles.right)}>
+                    <div className="col">
                         <p className={styles.title}>
-                            <span className={classNames(styles.step, styles.second)}>2</span>
+                            <span className={styles.num}>2</span>
                             用户登录后，根据三种不同情况更新应用的数据库用户表。
                         </p>
                         <div className={styles.guideBox}>
@@ -64,6 +65,21 @@ export default function () {
                                     找不到记录，则是新用户，新建一条记录（将返回的用户id赋给onlyid字段）。
                                 </li>
                             </ol>
+                        </div>
+                    </div>
+                </div>
+                <div className={classNames("row", styles.row3)}>
+                    <div className="col">
+                        <img src={migrate3} alt="migrate3" className={styles.img1} />
+                    </div>
+                    <div className="col">
+                        <p className={styles.title}>
+                            <span className={styles.num}>3</span>在 "我"
+                            页展示返回的用户信息，并增加修改账号信息的提示文案。
+                        </p>
+                        <div className={styles.guideBox}>
+                            <span className="material-icons">thumb_up</span>
+                            <p>建议文案：请使用唯ID APP修改本页用户信息。</p>
                         </div>
                     </div>
                 </div>
