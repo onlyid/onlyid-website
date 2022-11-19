d4a6c15ec9a743e6152dba8146bd787cab15ee98
ltb <liangtb@qq.com>
2022-11-19 16:49:29 +0800

    refactor: 移除@material-ui/icons（只使用font形式的icon）

--- src/components/home/Intro.js
+++ src/components/home/Intro.js
@@ -1,7 +1,6 @@
 import React from "react";
 import intro from "@site/static/img/home-intro.png";
 import styles from "./Intro.module.css";
-import { SentimentVeryDissatisfied } from "@material-ui/icons";
 
 export default function () {
     return (
@@ -13,11 +12,15 @@ export default function () {
                             <p className={styles.p1}>你的应用需要用户登录和用户管理功能，但...</p>
                             <ul className={styles.ul1}>
                                 <li>
-                                    <SentimentVeryDissatisfied />
+                                    <span className="material-icons">
+                                        sentiment_very_dissatisfied
+                                    </span>
                                     开发维护相关功能费时费力，手机号登录带来高昂短信成本。
                                 </li>
                                 <li>
-                                    <SentimentVeryDissatisfied />
+                                    <span className="material-icons">
+                                        sentiment_very_dissatisfied
+                                    </span>
                                     用户不情愿在你的应用注册，因为又要记忆多一个账号密码。
                                 </li>
                             </ul>
