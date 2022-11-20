d69f214e0ef2bcf407b8ab96c155763410e1e72c
ltb <liangtb@qq.com>
2022-11-21 00:58:10 +0800

    refactor: 优化home pro的文案和样式

--- src/components/home/Pro.js
+++ src/components/home/Pro.js
@@ -17,8 +17,7 @@ export default function () {
                     </div>
                     <div className={classNames("col", styles.col1)}>
                         <p>
-                            前平安集团智慧城市统一用户平台 PAI（Ping An
-                            Identity）总负责人创立并领导唯ID。
+                            前平安集团智慧城市统一用户平台负责人，创立唯ID并长期担任总负责人指引前进方向。
                         </p>
                     </div>
                 </div>
@@ -28,7 +27,7 @@ export default function () {
                     </div>
                     <div className={classNames("col", styles.col2)}>
                         <p>
-                            前招商银行风控反欺诈专家，打造支付级别安全风控系统，为每一次登录保驾护航。
+                            前招商银行高级风控工程师，打造支付级别安全风控系统，为用户每一次登录保驾护航。
                         </p>
                     </div>
                 </div>
@@ -39,7 +38,7 @@ export default function () {
                             <div>
                                 <h3>分布式云架构</h3>
                                 <p>
-                                    基于成熟的Java+Spring分布式技术，可承载数亿用户规模，支持数万人并发使用，全年可用时间高达
+                                    基于成熟的Java+Spring技术，可承载数千万用户规模，支持数万人并发使用，全年可用时间高达
                                     99.9% 。
                                 </p>
                             </div>
@@ -51,7 +50,7 @@ export default function () {
                             <div>
                                 <h3>全面技术支持</h3>
                                 <p>
-                                    提供全方位技术支持，覆盖售前咨询、集成顾问、使用帮助等。生产级别问题
+                                    提供全方位技术支持，覆盖售前咨询、集成顾问、使用帮助等；生产级别问题
                                     7×24 小时随时响应，全年无休。
                                 </p>
                             </div>
