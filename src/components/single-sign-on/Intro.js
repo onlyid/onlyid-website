9b96e843cd2a087568a5a80d0888096a2a44ba4f
ltb <liangtb@qq.com>
2023-04-20 02:39:37 +0800

    feat: sso页新增intro section

--- src/components/single-sign-on/Intro.js
+++ src/components/single-sign-on/Intro.js
@@ -1,50 +1,62 @@
 import React from "react";
 import styles from "./Intro.module.css";
-import intro from "@site/static/img/sso-intro.png";
-import classNames from "classnames";
 
 export default function () {
     return (
         <section className={styles.root}>
             <div className="container">
-                <h2 className="main-title">密码，一个就好</h2>
+                <p className={styles.p1}>现在应用一般使用的 自有+社交 登录方式存在不少弊端</p>
                 <div className="row">
-                    <div className={classNames("col", styles.left)}>
-                        <img src={intro} alt="intro" className={styles.img1} />
+                    <div className="col">
+                        <div>
+                            <div className={styles.sideBox}>
+                                <h4>企业端</h4>
+                                <div className={styles.divider} />
+                            </div>
+                            <div className={styles.item}>
+                                <h3>
+                                    <span className="material-icons" style={{ color: "#f08c10" }}>
+                                        sentiment_very_dissatisfied
+                                    </span>
+                                    开发麻烦
+                                </h3>
+                                <p>开发并维护自有+多个社交登录，会耗费开发者大量的时间和精力</p>
+                            </div>
+                            <div className={styles.item}>
+                                <h3>
+                                    <span className="material-icons" style={{ color: "#ef4d55" }}>
+                                        sentiment_very_dissatisfied
+                                    </span>
+                                    不够安全
+                                </h3>
+                                <p>开发自有登录涉及复杂的安全知识，稍有不慎就容易出现安全漏洞</p>
+                            </div>
+                        </div>
                     </div>
-                    <div className={classNames("col", styles.right)}>
+                    <div className="col">
                         <div>
-                            <h3 className={styles.title}>
-                                <span className={classNames("material-icons", styles.password)}>
-                                    password
-                                </span>
-                                一个密码
-                            </h3>
-                            <p className={styles.detail}>
-                                密码本身不是问题，密码太多才是问题。接入SSO，让用户用一个密码登录你和其他开发者的应用。
-                            </p>
-                        </div>
-                        <div>
-                            <h3 className={styles.title}>
-                                <span className={classNames("material-icons", styles.account)}>
-                                    portrait
-                                </span>
-                                一套账号
-                            </h3>
-                            <p className={styles.detail}>
-                                用户标准账号包括头像、昵称、邮箱和手机号等十个字段，供你的应用直接使用而不需要再单独维护。
-                            </p>
-                        </div>
-                        <div>
-                            <h3 className={styles.title}>
-                                <span className={classNames("material-icons", styles.fingerprint)}>
-                                    fingerprint
-                                </span>
-                                多种认证
-                            </h3>
-                            <p className={styles.detail}>
-                                除了传统的密码和验证码，还支持主动推送、扫码登录等多种认证方式，解放用户的同时也更加安全。
-                            </p>
+                            <div className={styles.sideBox}>
+                                <h4>用户端</h4>
+                                <div className={styles.divider} />
+                            </div>
+                            <div className={styles.item}>
+                                <h3>
+                                    <span className="material-icons" style={{ color: "#2299dd" }}>
+                                        sentiment_very_dissatisfied
+                                    </span>
+                                    容易迷茫
+                                </h3>
+                                <p>根据调研数据，超过六成用户会忘记之前是以哪种方式登录某应用</p>
+                            </div>
+                            <div className={styles.item}>
+                                <h3>
+                                    <span className="material-icons" style={{ color: "#3f51c5" }}>
+                                        sentiment_very_dissatisfied
+                                    </span>
+                                    难以记忆
+                                </h3>
+                                <p>自有登录注册过程繁琐，又要记忆多一个密码，用户使用意愿不高</p>
+                            </div>
                         </div>
                     </div>
                 </div>
